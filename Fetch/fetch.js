13b5178a5b9707dce33ebc2fbf7dae541008e55d
Rutvik <rutvikraut567@gmail.com>
2024-09-06 00:08:14 +0530

    Constructor Function

--- Fetch/fetch.js
+++
@@ -1,5 +0,0 @@
-// Fetch() method start the process of fetching a resource from network, returning a promise which is fullfilled once the response is available
-
-//fetch() promise only rejects when network error is encountered(usually when their is permission issue)
-
-//fetch() promise does not reject on HTTP errors(404, etc.). Instead then() handler must check the Response.ok and Response.status properties
